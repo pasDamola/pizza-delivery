923395bc91d8aa98de0b5162900ffdc13fc3b5d0
Oyinda David <oyetunmibidavid9@gmail.com>
2018-10-30 18:02:28 +0000

    create, delete and update apis completed

--- index.js
+++ index.js
@@ -2,18 +2,25 @@
 const express = require('express');
 const routes = require('./routes/api');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 // Set up express app
 const app = express();
 
+//connect to mongobd
+mongoose.connect('mongodb://localhost/users', { useNewUrlParser: true });
+mongoose.Promise = global.Promise;
 
 // body parser middleware
 app.use(bodyParser.json());
 
-
-
 // initialize all the routes 
 app.use('/api', routes);
+
+// error handling middleware
+app.use(function (err, req, res, next) {
+    res.status(422).send({ error: err.message });
+});
 
 //Start the server
 app.listen(process.env.port || 4000, function () {
--- routes/api.js
+++ routes/api.js
@@ -1,24 +1,33 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/users');
 
 // Get all the users from the database
-router.get('/users', function (req, res) {
+router.get('/users', function (req, res, next) {
     res.send('Hello World');
 });
 
 // Add new users to the database
-router.post('/users', function (req, res) {
-    res.send('Hello World');
+router.post('/users', function (req, res, next) {
+    User.create(req.body).then(function (user) {
+        res.send(user);
+    }).catch(next);
 });
 
 // Update a user in the database
-router.put('/users/:id', function (req, res) {
-    res.send('Hello World');
+router.put('/users/:id', function (req, res, next) {
+    User.findByIdAndUpdate({ _id: req.params.id }, req.body).then(function () {
+        User.findOne({ _id: req.params.id }).then(function (user) {
+            res.send(user);
+        })
+    });
 });
 
 // Delete a user from the database
-router.delete('/users/:id', function (req, res) {
-    res.send('Hello World');
+router.delete('/users/:id', function (req, res, next) {
+    User.findByIdAndRemove({ _id: req.params.id }).then(function (user) {
+        res.send(user);
+    });
 });
 
 module.exports = router;
